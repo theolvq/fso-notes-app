5c93f807de857583125d14e638cb36f5be5a4277
daawascript <70675996+daawascript@users.noreply.github.com>
2021-05-30 20:16:28 -0700

    created client side react app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send();
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server' });
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
