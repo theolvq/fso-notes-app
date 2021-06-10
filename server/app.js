f236acdd7272ce8ba12a37ac0c11935de739e2be
daawascript <70675996+daawascript@users.noreply.github.com>
2021-06-10 10:39:43 -0700

    implemented login with json web token

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
+const loginRouter = require('./controllers/login');
 
 logger.info('connecting to', config.MONGODB_URI);
 
@@ -32,6 +33,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
