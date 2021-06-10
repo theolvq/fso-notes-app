6138ebc520a8051ae4b02b8dac3443113ba0f76e
daawascript <70675996+daawascript@users.noreply.github.com>
2021-06-10 11:06:31 -0700

    added error handling and only logged in users can add new notes

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -21,7 +21,9 @@ loginRouter.post('/', async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 60 * 60,
+  });
 
   res.status(200).send({ token, username: user.username, name: user.name });
 });
--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -1,6 +1,14 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = req => {
+  const authorization = req.get('authorization');
+  return authorization && authorization.toLowerCase().startsWith('bearer ')
+    ? authorization.substring(7)
+    : null;
+};
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
@@ -14,8 +22,12 @@ notesRouter.get('/:id', async (req, res, next) => {
 
 notesRouter.post('/', async (req, res, next) => {
   const body = req.body;
-
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(req);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
